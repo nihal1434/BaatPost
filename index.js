a6ac16c045f97fcfe6cfc98b5e167d400b199e41
nihal1434 <nihalonly772@gmail.com>
2025-07-28 13:34:16 +0530

    photo not loading

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ function handlePostBtnClick() {
   if (postInput.value) {
     postsData.unshift({
       handle: `@Nyl_Sarcastic 😏`,
-      profilePic: `./img/nyl.png`,
+      profilePic: `nyl.png`,
       likes: 0,
       reposts: 0,
       postText: postInput.value,
